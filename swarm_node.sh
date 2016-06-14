1997fb910345e55f1abf7a978ce849bf9e1a6fe9
Ubuntu <ubuntu@ip-172-31-29-38.eu-central-1.compute.internal>
2016-06-14 06:11:39 +0000

    reverted consul - does not work

--- swarm_node.sh
+++ swarm_node.sh
@@ -3,5 +3,5 @@ sudo docker rm -v $(sudo docker ps -aq)
 
 ADVERTISE_ADDRESS=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 
-sudo docker run -d --net=reservierung swarm join --advertise $ADVERTISE_ADDRESS:2375 consul://consul:8500
+sudo docker run -d --net=reservierung swarm join --advertise $ADVERTISE_ADDRESS:2375 consul://$1:8500
 
